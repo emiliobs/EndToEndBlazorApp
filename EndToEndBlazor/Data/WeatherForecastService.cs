694ad55b362c1f14157436d3a07be7884680d262
emilio barrera <barrera_emilio@hotmail.com>
2019-08-20 17:41:32 +0100

    Edit Register Ready

--- EndToEndBlazor/Data/WeatherForecastService.cs
+++ EndToEndBlazor/Data/WeatherForecastService.cs
@@ -40,6 +40,31 @@ namespace EndToEndBlazor.Data
             return Task.FromResult(objWeatherForecast);
         }
 
+        public Task<bool> UpdateForescastAsync(WeatherForecast weatherForecast)
+        {
+            using (var context = new EndtoendblazorContext())
+            {
+                var existingWeatherforescast = context.WeatherForecast.Where(f => f.Id.Equals(weatherForecast.Id)).FirstOrDefault();
+
+                if (existingWeatherforescast != null)
+                {
+                    existingWeatherforescast.Date = weatherForecast.Date;
+                    existingWeatherforescast.Summary = weatherForecast.Summary;
+                    existingWeatherforescast.TemperatureC = weatherForecast.TemperatureC;
+                    existingWeatherforescast.TemperatureF = weatherForecast.TemperatureF;
+
+                    context.SaveChanges();
+                }
+                else
+                {
+                    return Task.FromResult(true);
+                }
+
+
+                return Task.FromResult(true);
+
+            }
+        }
 
     }
 
